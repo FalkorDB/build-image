cb8609b64aa3dbcd15ace75fc8d3eebf12f0a7f2
Emily Matheys <emilymatt96@gmail.com>
2024-07-17 15:34:39 +0300

    Set all other llvm tools we might need during builds etc

--- set_alternatives.sh
+++ set_alternatives.sh
@@ -11,7 +11,15 @@ update-alternatives --install /usr/bin/ld ld "/usr/bin/lld-${LLVM_VERSION}" 100
 update-alternatives --install /usr/bin/lldb lldb "/usr/bin/lldb-${LLVM_VERSION}" 100
 update-alternatives --install /usr/bin/llvm-config llvm-config "/usr/bin/llvm-config-${LLVM_VERSION}" 100
 update-alternatives --install /usr/bin/llvm-cov llvm-cov "/usr/bin/llvm-cov-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-nm llvm-nm "/usr/bin/llvm-nm-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-objdump llvm-objdump "/usr/bin/llvm-objdump-${LLVM_VERSION}" 100
 update-alternatives --install /usr/bin/llvm-profdata llvm-profdata "/usr/bin/llvm-profdata-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-ranlib llvm-ranlib "/usr/bin/llvm-ranlib-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-readelf llvm-readelf "/usr/bin/llvm-readelf-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-readobj llvm-readobj "/usr/bin/llvm-readobj-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-size llvm-size "/usr/bin/llvm-size-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-strip llvm-strip "/usr/bin/llvm-strip-${LLVM_VERSION}" 100
+update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer "/usr/bin/llvm-symbolizer-${LLVM_VERSION}" 100
 
 # Rust needs this to find libclang when using the cc crate
 ln -sf "/usr/lib/$(gcc -dumpmachine)/libclang-${LLVM_VERSION}.so.1" /usr/lib/"$(gcc -dumpmachine)"/libclang.so
